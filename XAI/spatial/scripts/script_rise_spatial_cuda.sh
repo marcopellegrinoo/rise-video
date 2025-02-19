bf11d300a3dc8047c96841c4d7fbf4f0d78cbf08
marcopellegrinoo <88186815+marcopellegrinoo@users.noreply.github.com>
2025-02-19 16:06:53 +0100

    Update script_rise_spatial_cuda.sh

--- XAI/spatial/scripts/script_rise_spatial_cuda.sh
+++ XAI/spatial/scripts/script_rise_spatial_cuda.sh
@@ -12,9 +12,12 @@
 #SBATCH --qos=normal                    # quality of service (optional)
 
 export PYTHONUNBUFFERED=TRUE
+
 module purge
 module load cuda/12.2
 module load cudnn/8.9
+
+export XLA_FLAGS=--xla_gpu_cuda_data_dir=/leonardo/prod/opt/compilers/cuda/12.2/none/nvvm/libdevice
 
 source $WORK/Water_Resources/env/bin/activate
 cd $WORK/Water_Resources/rise-video/XAI/spatial/scripts
