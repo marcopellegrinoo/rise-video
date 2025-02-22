4017c9aeacfd1592b516223b86cf26a9ec4628d6
marcopellegrinoo <88186815+marcopellegrinoo@users.noreply.github.com>
2025-02-22 17:50:41 +0100

    Update script_rise_spatial_additive_uniform_noise.sh

--- XAI/spatial/scripts/script_rise_spatial_additive_uniform_noise.sh
+++ XAI/spatial/scripts/script_rise_spatial_additive_uniform_noise.sh
@@ -5,8 +5,8 @@
 #SBATCH --gpus-per-node=1
 #SBATCH --time=24:00:00                 # time limits: here 1 hour
 #SBATCH --mem=80000                     # total memory per node requested in GB (optional)
-#SBATCH --error=/leonardo_scratch/fast/try25_pellegrino/results/logs/rise_spatial.err               # standard error file
-#SBATCH --output=/leonardo_scratch/fast/try25_pellegrino/results/logs/rise_spatial.out              # standard output file
+#SBATCH --error=/leonardo_scratch/fast/try25_pellegrino/results/logs/rise_spatial_additive_uniform_noise.err               # standard error file
+#SBATCH --output=/leonardo_scratch/fast/try25_pellegrino/results/logs/rise_spatial_additive_uniform_noise.out              # standard output file
 #SBATCH --account=try25_pellegrino      # account name
 #SBATCH --partition=boost_usr_prod      # partition name
 #SBATCH --qos=normal                    # quality of service (optional)
