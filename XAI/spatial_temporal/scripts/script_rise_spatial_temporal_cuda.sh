130aa5615d0f708fc2ae90434e6ade4b44fba762
marcopellegrinoo <88186815+marcopellegrinoo@users.noreply.github.com>
2025-02-26 17:09:59 +0100

    Update script_rise_spatial_temporal_cuda.sh

--- XAI/spatial_temporal/scripts/script_rise_spatial_temporal_cuda.sh
+++ XAI/spatial_temporal/scripts/script_rise_spatial_temporal_cuda.sh
@@ -12,9 +12,6 @@
 #SBATCH --qos=normal                    # quality of service (optional)
 
 export PYTHONUNBUFFERED=TRUE
-module purge 
-
-module load cuda@11.8.0
 
 source $WORK/Water_Resources/env/bin/activate
 cd $WORK/Water_Resources/rise-video/XAI/spatial_temporal/scripts
